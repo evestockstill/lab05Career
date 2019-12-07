268968cdd19144cffa89dd838de5ecc68b82fe93
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-06 16:18:32 -0800

    create route

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,13 +1,55 @@
+require('dotenv').config();
 const request = require('supertest');
 const app = require('../lib/app');
+const connect = require('../config/db');
+const colors = require('colors');
+const mongoose = require('mongoose');
+
 
 
 describe('application routes', () => {
-  it('has a home route that says hello everyone', () => {
-    return request(app)
-      .get('/')
-      .then(res => {
-        expect(res.body).toEqual({ text: 'hello' });
-      });
+
+  beforeAll(() => {
+    connect()
   });
+
+  afterAll(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  
+  it('can create a bar using post', () => {
+    return request(app)
+    .post('/bars')
+    .send({
+      name: 'some bar',
+      phone: '555-555-5555',
+      age: 21
+    })
+    .then(res => {
+      console.log('recieved response in test');
+      expect(res.body).toEqual({
+        _id: expect.any(String),
+        name: 'some bar',
+        phone: '555-555-5555',
+        age: 21,
+        __v: 0
+      })
+    })
+  });
+  
+    it('can get all bars using get method', () => {
+      return request(app)
+        .get('/bars')
+        .then(res => {
+          expect(res.body).toEqual([{
+            _id: expect.any(String),
+            name: 'some bar',
+            phone: '555-555-5555',
+            age: 21,
+            __v: 0
+          }]);
+          expect(res.body).toHaveLength(1);
+        });
+    });
+
 });
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-
-const dotenv = require('dotenv');
+const app = require('./lib/app')
+require('dotenv').config();
 const colors = require('colors');
-const connectDB = require('./config/db');
+require('./config/db')();
 // route files
-dotenv.config({ path: './config/.env' });
-connectDB();
+
 
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
-  PORT,
+app.listen(PORT, () => {
   console.log(`server running on ${PORT}`.yellow.bold)
-);
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`error: ${err.message}`.red);
-  server.close(() => process.exit(1));
 });
+
+
